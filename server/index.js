d3cccd965cd9a98d9e8e29b99e34936b5d58fcb0
Mohammad Ali Ashraf <ashraf182003@gmail.com>
2024-01-29 11:24:14 +0100

    firstcommit

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import { dbName, port, uri } from "./lib/env-vars.js";
+import express from "express";
+//import routes from "./routes/index.js";
+import connectDB from "./lib/mongodb.js";
+import "dotenv/config";
+
+const app = express();
+
+app.use(express.json())
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+
+  connectDB();
+});
