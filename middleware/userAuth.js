27ea31efc23f77a7889e56bab4de1d04f4b2204f
KostasBzn <kostasbouzianis@gmail.com>
2024-01-29 13:10:30 +0100

    user auth with token ready

--- middleware/userAuth.js
+++ middleware/userAuth.js
@@ -1,1 +1,15 @@
 import jwt from "jsonwebtoken";
+
+const auth = (req, res, next) => {
+  const token = rew.headers.authorisation || req.headers.Authorisation;
+
+  try {
+    const decodedToken = jwt.verify(token, process.env.SECRET_TOKEN_KEY);
+    req.user = decodedToken;
+    next();
+  } catch (error) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+export default auth;
